4fc3243fb60b972ad3f4e917d75bb3065cd87e4c
swetha1223 <24bct153@kgcas.com>
2025-07-23 10:45:34 +0530

    Add files via upload

---
+++ day03.js
@@ -0,0 +1,14 @@
+// variables decalred outside any function or block are in the global scope.
+//they are accesed anywhere 
+//globalscope
+
+    
+//function scope
+let a="10";
+function Say(){
+    let name="javascript";
+    console.log("good morning");
+    console.log("bye");
+
+}
+Say();
